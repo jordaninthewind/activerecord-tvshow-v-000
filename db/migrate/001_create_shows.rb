ae23b42c6a7c8dfc5ef93cf3b8b3d59318910169
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 22:46:09 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,5 +1,9 @@
 class CreateShows < ActiveRecord::Migration
   def change
-
+    create_table :shows do |el|
+      el.string :name
+      el.string :network
+      el.string :day
+      el.integer :rating
   end
 end
