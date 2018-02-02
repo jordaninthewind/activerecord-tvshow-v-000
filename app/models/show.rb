23badf9c953fd07f0c4eb1fefae6093c116ab596
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 23:36:07 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,6 +6,6 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     highest_rating = self.highest_rating
-    self.find_by :rating highest_rating
+    Show.find_by :rating highest_rating
   end
 end
